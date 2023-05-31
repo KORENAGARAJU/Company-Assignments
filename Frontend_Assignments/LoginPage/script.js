b26f4dd1307cd432ba34389ccac56e610f7c4919
KORE NAGARAJU <nagaraj13562001@gmail.com>
2023-05-31 14:02:03 +0530

    Project-1 completed

--- Frontend_Assignments/LoginPage/script.js
+++ Frontend_Assignments/LoginPage/script.js
@@ -23,6 +23,104 @@ passwordHide.forEach(eyeIcon=>{
     }) ;
 }) ;
 
-//to render login and sign-up page 
-let login = true ;
+//change icon for login page to visible password
+const loginHideIcon = document.querySelector('.hide-eye-login') ;
+const loginPassInput = document.querySelector('.login-password') ;
+loginHideIcon.addEventListener('click',()=>{
+    if(loginPassInput.type === 'password'){
+        loginPassInput.type = 'text' ;
+        loginHideIcon.classList.replace('uil-eye-slash','uil-eye') ;
+    }
+    else{
+        loginPassInput.type = 'password' ;
+        loginHideIcon.classList.replace('uil-eye','uil-eye-slash') ;
+    }
+})
+
+// form vadilitation for Registration page 
+const errorUserName = document.querySelector('.reg-name') ;
+const errorPass = document.querySelector('.reg-password') ;
+const errorEmail = document.querySelector('.reg-email') ;
+
+const submitBtn = document.querySelector('.SignupBtn')
+submitBtn.addEventListener('click',(e)=>{
+     e.preventDefault() ;
+    //checking for user name 
+    if(reg_userName.value === ''){
+        const errorName = 'Name is required'
+        errorUserName.textContent = errorName ;
+        reg_userName.style.borderBottomColor = 'red'
+    }
+    else{
+        errorUserName.textContent = '' ;
+        reg_userName.style.borderBottomColor = 'green'
+    }
+    //checking for email 
+    if(regEmail.value === ''){
+        const  errorEmailName = 'Email is requried'
+        errorEmail.textContent = errorEmailName ;
+        regEmail.style.borderBottomColor = 'red'
+    }
+    else{
+        errorEmail.textContent = '' ;
+        regEmail.style.borderBottomColor = 'green'
+    }
+
+    //checking for password to contains more than 8 character
+    if(regPassword.value === ''){
+        const errorPassName = 'Password is required' ;
+        errorPass.textContent = errorPassName ;
+        regPassword.style.borderBottomColor = 'red'
+    }
+    else if(regPassword.value.length < 8){
+        errorPass.textContent = 'Required more than 8 characters' ;
+        regPassword.style.borderBottomColor = 'red'
+    }
+    else{
+        errorPass.textContent = '' ;
+        regPassword.style.borderBottomColor = 'green'
+    }
+})
+
+
+
+//checking for login vadilation
+const reg_userName = document.getElementById('reg-name-id') ;
+const regEmail = document.getElementById('reg-email-id') ;
+const regPassword = document.getElementById('reg-pass-id') ;
+//------------------
+//form valid 
+const loginEmail = document.querySelector('.login-email') ;
+const errorEmailLogin = document.querySelector('.login-email-error') ;
+const loginPassword = document.querySelector('.login-password') ;
+const errorLoginPassword = document.querySelector('.login-Errpassword')
+const loginBtn = document.querySelector('.loginBtn') ;
+loginBtn.addEventListener('click',(e)=>{
+    e.preventDefault() ;
+    if(loginEmail.value === ''){
+        const  errorEmailName = 'Email is requried'
+        errorEmailLogin.textContent = errorEmailName ;
+        loginEmail.style.borderBottomColor = 'red'
+    }
+    else{
+        errorEmailLogin.textContent = '' ;
+        loginEmail.style.borderBottomColor = 'green'
+    }
+
+    //checking for password to contains more than 8 character
+    if(loginPassword.value === ''){
+        const errorPassName = 'Password is required' ;
+        errorLoginPassword.textContent = errorPassName ;
+        loginPassword.style.borderBottomColor = 'red'
+    }
+    else if(loginPassword.value.length < 8){
+        errorLoginPassword.textContent = 'Required more than 8 characters' ;
+        loginPassword.style.borderBottomColor = 'red'
+    }
+    else{
+        errorLoginPassword.textContent = '' ;
+        loginPassword.style.borderBottomColor = 'green'
+    }
+
+});
 
